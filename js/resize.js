33dc71a95fa32e231582dc9057f52fc9040e4d10
Viriy <541239223@qq.com>
2018-08-06 14:08:19 +0800

    test

--- js/resize.js
+++ js/resize.js
@@ -1,5 +1,5 @@
 /*to load use reset-screen-size*/
-(function(document){var dcl=document.documentElement,wh;function setRootRem(){ww=dcl.clientWidth;wh=dcl.clientHeight;if(wh<ww*2-wh*0.037){dcl.style.fontSize=100*(wh/1334)+'px'}else{dcl.style.fontSize=100*(ww/750)+'px'}}setRootRem();document.addEventListener('DOMContentLoaded',setRootRem,false);window.addEventListener('resize',setRootRem,false)})(document);
+// (function(document){var dcl=document.documentElement,wh;function setRootRem(){ww=dcl.clientWidth;wh=dcl.clientHeight;if(wh<ww*2-wh*0.037){dcl.style.fontSize=100*(wh/1334)+'px'}else{dcl.style.fontSize=100*(ww/750)+'px'}}setRootRem();document.addEventListener('DOMContentLoaded',setRootRem,false);window.addEventListener('resize',setRootRem,false)})(document);
 
 // (function(document) {
 //     var dcl = document.documentElement,wh;
@@ -14,47 +14,45 @@
 
 
 
-// (function(doc, win) {
-// //      用原生方法获取用户设置的浏览器的字体大小(兼容ie)
-//         if(doc.documentElement.currentStyle) {
-//             var user_webset_font=doc.documentElement.currentStyle['fontSize'];
-//         }
-//         else {
-//             var user_webset_font=getComputedStyle(doc.documentElement,false)['fontSize'];
-//         }
+(function(doc, win) {
+//      用原生方法获取用户设置的浏览器的字体大小(兼容ie)
+        if(doc.documentElement.currentStyle) {
+            var user_webset_font=doc.documentElement.currentStyle['fontSize'];
+        }
+        else {
+            var user_webset_font=getComputedStyle(doc.documentElement,false)['fontSize'];
+        }
 
-//         var dcl = document.documentElement,wh;
+//      取整后与默认16px的比例系数
+        var xs=parseFloat(user_webset_font)/16;
+//      设置rem的js设置的字体大小
+        var view_jsset_font,result_font;
+        var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        clientWidth,
+        recalc = function() {
+            clientWidth = docEl.clientWidth;
+            clientHeight = docEl.clientHeight;
 
-// //      取整后与默认16px的比例系数
-//         var xs=parseFloat(user_webset_font)/16;
-// //      设置rem的js设置的字体大小
-//         var view_jsset_font,result_font;
-//         var docEl = doc.documentElement,
-//         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-//         clientWidth,
-//         recalc = function() {
-//             clientWidth = docEl.clientWidth;
-//             clientHeight = docEl.clientHeight;
-
-//             if(!clientWidth) return;
-//             if(!doc.addEventListener) return;
-//             if(clientHeight < clientWidth * 2 - clientHeight * 0.037){
-//                 view_jsset_font=100 * (clientHeight /1334);
-//                 result_font=view_jsset_font/xs;
-//                 docEl.style.fontSize = result_font + 'px';
-//                 }
-//             else{
-//                 //设置rem的js设置的字体大小
-//                 view_jsset_font=75 * (clientWidth / 750);
-// //              最终的字体大小为rem字体/系数
-//                 result_font=view_jsset_font/xs;
-// //              设置根字体大小
-//                 docEl.style.fontSize = result_font + 'px';
-//             }
-//         };
-//     win.addEventListener(resizeEvt, recalc, false);
-//     doc.addEventListener('DOMContentLoaded', recalc, false);
-// })(document, window);
+            if(!clientWidth) return;
+            if(!doc.addEventListener) return;
+            if(clientHeight < clientWidth * 2 - clientHeight * 0.037){
+                view_jsset_font=100 * (clientHeight /1334);
+                result_font=view_jsset_font/xs;
+                docEl.style.fontSize = result_font + 'px';
+                }
+            else{
+                //设置rem的js设置的字体大小
+                view_jsset_font=75 * (clientWidth / 750);
+//              最终的字体大小为rem字体/系数
+                result_font=view_jsset_font/xs;
+//              设置根字体大小
+                docEl.style.fontSize = result_font + 'px';
+            }
+        };
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
 
 /*判断手机系统,兼容屏幕*/
 var phoneWidth=parseInt(window.screen.width);var phoneScale=phoneWidth/750;var ua=navigator.userAgent;if(/Android (\d+\.\d+)/.test(ua)){var version=parseFloat(RegExp.$1);if(version>2.3){document.write('<meta name="viewport" content="width=750, minimum-scale='+phoneScale+', maximum-scale='+phoneScale+'">')}else{document.write('<meta name="viewport" content="width=750,user-scalable=no">')}}else{document.write('<meta name="viewport" content="width=750, user-scalable=no">')};
