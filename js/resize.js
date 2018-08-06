c9fa3cae68dfcd57d6b9ecda38a4cef8ca4e837a
Viriy <541239223@qq.com>
2018-08-06 11:41:43 +0800

    test

--- js/resize.js
+++ js/resize.js
@@ -1,6 +1,19 @@
 /*to load use reset-screen-size*/
 // (function(document){var dcl=document.documentElement,wh;function setRootRem(){ww=dcl.clientWidth;wh=dcl.clientHeight;if(wh<ww*2-wh*0.037){dcl.style.fontSize=100*(wh/1334)+'px'}else{dcl.style.fontSize=100*(ww/750)+'px'}}setRootRem();document.addEventListener('DOMContentLoaded',setRootRem,false);window.addEventListener('resize',setRootRem,false)})(document);
 
+
+(function(document) {
+    var dcl = document.documentElement,
+        wh;
+
+    function setRootRem() { ww = dcl.clientWidth;
+        wh = dcl.clientHeight;
+        if (wh < ww * 2 - wh * 0.037) { dcl.style.fontSize = 100 * (wh / 1334) + 'px' } else { dcl.style.fontSize = 100 * (ww / 750) + 'px' } }
+    setRootRem();
+    document.addEventListener('DOMContentLoaded', setRootRem, false);
+    window.addEventListener('resize', setRootRem, false) })(document);
+
+
 (function(doc, win) {
 //      用原生方法获取用户设置的浏览器的字体大小(兼容ie)
         if(doc.documentElement.currentStyle) {
@@ -17,9 +30,23 @@
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         clientWidth,
         recalc = function() {
-            clientWidth = docEl.clientWidth;
             if(!clientWidth) return;
             if(!doc.addEventListener) return;
+        	var dcl = document.documentElement,wh;
+            clientWidth = docEl.clientWidth;
+            ww = dcl.clientWidth;
+        	wh = dcl.clientHeight;
+            if (wh < ww * 2 - wh * 0.037) {
+            	dcl.style.fontSize = 100 * (wh / 1334) + 'px'
+            } else {
+//              设置rem的js设置的字体大小
+                view_jsset_font=100 * (clientWidth / 750);
+//              最终的字体大小为rem字体/系数
+                result_font=view_jsset_font/xs;
+//              设置根字体大小
+                docEl.style.fontSize = result_font + 'px';
+            }
+
             //if(clientWidth<540){
 //              设置rem的js设置的字体大小
                 view_jsset_font=100 * (clientWidth / 750);
