054fd32f05c29556e3e2f99110d507b417cc3672
Viriy <541239223@qq.com>
2018-08-06 14:17:21 +0800

    test

--- js/resize.js
+++ js/resize.js
@@ -1,19 +1,6 @@
 /*to load use reset-screen-size*/
 // (function(document){var dcl=document.documentElement,wh;function setRootRem(){ww=dcl.clientWidth;wh=dcl.clientHeight;if(wh<ww*2-wh*0.037){dcl.style.fontSize=100*(wh/1334)+'px'}else{dcl.style.fontSize=100*(ww/750)+'px'}}setRootRem();document.addEventListener('DOMContentLoaded',setRootRem,false);window.addEventListener('resize',setRootRem,false)})(document);
 
-// (function(document) {
-//     var dcl = document.documentElement,wh;
-
-//     function setRootRem() { ww = dcl.clientWidth;
-//         wh = dcl.clientHeight;
-//         if (wh < ww * 2 - wh * 0.037) { dcl.style.fontSize = 100 * (wh / 1334) + 'px' } else { dcl.style.fontSize = 100 * (ww / 750) + 'px' }
-//     }
-//     setRootRem();
-//     document.addEventListener('DOMContentLoaded', setRootRem, false);
-//     window.addEventListener('resize', setRootRem, false) })(document);
-
-
-
 (function(doc, win) {
 //      用原生方法获取用户设置的浏览器的字体大小(兼容ie)
         if(doc.documentElement.currentStyle) {
@@ -43,7 +30,7 @@
                 }
             else{
                 //设置rem的js设置的字体大小
-                view_jsset_font=75 * (clientWidth / 750);
+                view_jsset_font=100 * (clientWidth / 750);
 //              最终的字体大小为rem字体/系数
                 result_font=view_jsset_font/xs;
 //              设置根字体大小
